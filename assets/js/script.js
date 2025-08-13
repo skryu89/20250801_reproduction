7fe3e5891d42f916acbbc96588e9cce8555f8a53
skryu89 <mtjapwd@gmail.com>
2025-08-14 01:01:44 +0900

    メインビジュアルの追加

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
   $('.company-seniors').slick({
-    slidesToShow: 3,        // 一度に表示する枚数
-    slidesToScroll: 1,      // スクロールする枚数
-    autoplay: true,         // 自動再生
-    autoplaySpeed: 0,       // 間隔（0 にすると常に流れる）
-    speed: 3000,            // スクロール速度（ms）
-    cssEase: '5000',      // 一定速度で流れる
-    infinite: true,         // 無限ループ
-    arrows: false,          // 矢印非表示
-    pauseOnHover: false,    // ホバーしても止めない
-    variableWidth: true    // 画像幅に応じて可変
+    slidesToShow: 3,        
+    slidesToScroll: 1,      
+    autoplay: true,        
+    autoplaySpeed: 0,       
+    speed: 3000,            
+    cssEase: '5000',      
+    infinite: true,         
+    arrows: false,          
+    pauseOnHover: false,    
+    variableWidth: true    
   });
 });
 
