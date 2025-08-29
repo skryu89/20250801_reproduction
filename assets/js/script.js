ea916ad64baa2b1bfdccd8ec58a3a618aa30a22b
skryu89 <mtjapwd@gmail.com>
2025-08-29 20:17:32 +0900

    募集要項メインビジュアル追加

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,4 @@
-$(document).ready(function () {
-  $('.company-seniors').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 0,
-    speed: 7000,
-    cssEase: 'linear',
-    infinite: true,
-    arrows: false,
-    pauseOnHover: false,
-    variableWidth: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          variableWidth: false
-        }
-      }
-    ]
-  });
-
-  $(".hamburger").on("click", function () {
+$(".hamburger").on("click", function () {
     $(this).toggleClass("open");     
     $(".nav-menu").fadeToggle();  
     $("body").toggleClass("menu-open");
@@ -48,4 +25,3 @@ function updateSlide() {
 window.addEventListener('load', updateSlide);
 window.addEventListener('resize', updateSlide);
 
-});
